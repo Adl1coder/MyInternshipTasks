df3809e1e7af99809d6e4ab53a5ee3903cfaf302
Adl1coder <adilegunngorr@gmail.com>
2023-09-15 20:12:55 +0300

    Update

---
+++ KOTLIN.kt
@@ -0,0 +1,13 @@
+class KOTLIN {
+
+        fun main(args: Array<String>) {
+
+            println("Ben Adile Güngör. Fırat'ta okuyorum." +
+                    "AHB Bilişim şirketinde kısa dönem zorunlu stajımı yapıyorum" +
+                    "Kotlin ile  uygulamalar geliştiriyorum. " +
+                    "Android geliştirici olarak devam ediyorum. " +
+                    "Staj süresince kodladığım herşeyi paylaşmayı düşünüyorum")
+        }
+    }
+
+}
